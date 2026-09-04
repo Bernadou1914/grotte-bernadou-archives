38d22c977468b705d2c4d700bbc02bf59a8df68c
Bernadou1914 <bernadoucazals@gmail.com>
2026-09-04 18:37:01 +0200

    Create layout.tsx

---
+++ app/app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css"
+
+export const metadata = {
+  title: "Archives des graffitis de la Grotte Bernadou",
+  description: "Inventaire et consultation des archives",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  )
+}
